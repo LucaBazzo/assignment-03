a84f740afc3112711e3a99d5cb5fb1f9f8c009c6
LucaBazzo <luca.bazzocchi2@studio.unibo.it>
2022-03-27 14:59:47 +0200

    actor system added

--- assignment-03-actors/src/main/scala/pcd/assignment03/main/Main.scala
+++ assignment-03-actors/src/main/scala/pcd/assignment03/main/Main.scala
@@ -1,5 +1,8 @@
 package pcd.assignment03.main
 
+import akka.NotUsed
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorSystem, Behavior}
 import pcd.assignment03.concurrency.WordsBagFilling
 import pcd.assignment03.view.View
 
@@ -9,23 +12,33 @@ object Main {
   private var DEFAULT_IGNORED_PATH = ".\\ignored\\ignored.txt"
   private var DEFAULT_N_WORDS = 5
 
-  def main(args:Array[String]): Unit = {
-    if(args.length == 3) {
-      DEFAULT_PDF_PATH = args(0)
-      DEFAULT_IGNORED_PATH = args(1)
-      DEFAULT_N_WORDS = args(2).toInt
+  def apply(initialConfig: Array[String]): Behavior[NotUsed] = Behaviors.setup { context =>
+
+    if(initialConfig.length == 3) {
+      DEFAULT_PDF_PATH = initialConfig(0)
+      DEFAULT_IGNORED_PATH = initialConfig(1)
+      DEFAULT_N_WORDS = initialConfig(2).toInt
     }
 
     val weight: Int = 700
     val height: Int = 400
 
     val view: View = new View(weight, height, DEFAULT_PDF_PATH,
-      DEFAULT_IGNORED_PATH, DEFAULT_N_WORDS);
-    val wordsBag: WordsBagFilling = new WordsBagFilling();
+      DEFAULT_IGNORED_PATH, DEFAULT_N_WORDS)
+    val wordsBag: WordsBagFilling = new WordsBagFilling()
 
-    val controller: Controller = new Controller(view, wordsBag);
-    view.addListener(controller);
-    view.display();
+    val controller: Controller = new Controller(view, wordsBag)
+    view.addListener(controller)
+    view.display()
+
+    Behaviors.empty
+  }
+
+
+
+
+  def main(args:Array[String]): Unit = {
+    ActorSystem(Main(args), "Concurrency")
   }
 
 }
