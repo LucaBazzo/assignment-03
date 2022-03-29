5068cf2f573981505a0631735f4177a293019f73
Giacomo Casadei <giacomo.casadei12@studio.unibo.it>
2022-03-29 16:51:06 +0200

    Converted MasterActor into an effective Actor

--- assignment-03-actors/src/main/scala/pcd/assignment03/tasks/MasterActor.scala
+++ assignment-03-actors/src/main/scala/pcd/assignment03/tasks/MasterActor.scala
@@ -25,20 +25,6 @@ object MasterActor {
   case class Start() extends MasterMessage
   case class MaxWordsResult(result: Option[(Integer, List[(String, Integer)])]) extends MasterMessage
 
-  def apply(taskType: String, view: ActorRef[ViewMessage], pdfDirectory: File, forbidden: File,
-            wordsBag: ActorRef[Command], stopMonitor: StopMonitor, numTasks: Int,
-            nWords: Int): Behavior[MasterMessage] =
-    Behaviors.setup { context =>
-      new MasterActor(taskType, view, pdfDirectory, forbidden, wordsBag, stopMonitor, numTasks,
-        nWords, context).start
-    }
-}
-
-
-class MasterActor(val taskType: String, val view: ActorRef[ViewMessage], val pdfDirectory: File, val forbidden: File,
-                  var wordsBag: ActorRef[Command], val stopMonitor: StopMonitor, var numTasks: Int,
-                  val nWords: Int, var context: ActorContext[MasterMessage]) {
-
   private val WAITING_TIME = 10
 
   private var forbiddenList: List[String] = List.empty
@@ -52,60 +38,69 @@ class MasterActor(val taskType: String, val view: ActorRef[ViewMessage], val pdf
   private var workCompleted = false
   private var startTime = 0L
 
-  private val start: Behavior[MasterMessage] = Behaviors.receiveMessagePartial {
-    case Start() =>
-      run()
-      Behaviors.same
-  }
+  private var taskType: String = ""
 
-  def run(): Unit = {
-    this.processPDF()
-    if (!stopMonitor.isStopped)
-      this.mostFrequentWords()
-  }
+  def apply(taskType: String, view: ActorRef[ViewMessage], pdfDirectory: File, forbidden: File,
+            wordsBag: ActorRef[Command], stopMonitor: StopMonitor, numTasks: Int,
+            nWords: Int): Behavior[MasterMessage] =
+    Behaviors.receive { (ctx, message) =>
+      message match {
+        case Start() => this.taskType = taskType
+          log(pdfDirectory.listFiles().length.toString)
+          this.processPDF(forbidden, view, pdfDirectory.listFiles(), stopMonitor)
+          if (!stopMonitor.isStopped)
+            this.mostFrequentWords(ctx, nWords, wordsBag, view, numTasks, stopMonitor)
 
-  private def processPDF(): Unit = {
+        case _ =>
+      }
+
+      Behaviors.empty
+
+    }
+
+  private def processPDF(forbidden: File, view: ActorRef[ViewMessage], pdfDirectory: Array[File],
+                        stopMonitor: StopMonitor): Unit = {
     this.startTime = System.currentTimeMillis()
 
     try {
       var reader: Scanner = new Scanner(forbidden)
-      while (reader.hasNextLine()) {
+      while (reader.hasNextLine) {
         var data: String = reader.nextLine()
         forbiddenList = data :: forbiddenList
       }
       reader.close()
     } catch {
-      case e: FileNotFoundException => {
+      case e: FileNotFoundException =>
         log("An error occurred.")
         e.printStackTrace()
-      }
+
     }
 
     view ! ChangeState("Getting PDF...")
 
     log("Wait completion")
 
-    pdfDirectory.listFiles().foreach(pdfFile => {
+    pdfDirectory.foreach(pdfFile => {
       val task: PDFExtractTask = new PDFExtractTask(forbiddenList, pdfFile, stopMonitor)
       taskList.add(task)
     })
 
-    view ! ChangeState("PDF Processing...");
+    view ! ChangeState("PDF Processing...")
 
     try {
       extractResults = executor.invokeAll(taskList)
     } catch {
-      case e: Exception => {
+      case e: Exception =>
         stopMonitor.stop()
         log("Interrupted")
         view ! ChangeState("Interrupted")
-      }
+
     }
 
     try {
       extractResults.forEach(future => breakable {
-        while(!this.stopMonitor.isStopped || future.isDone)
-          if(this.stopMonitor.isStopped)
+        while(!stopMonitor.isStopped || future.isDone)
+          if(stopMonitor.isStopped)
             throw new Exception()
           else {
             future.get().foreach(string => stringList = string :: stringList)
@@ -113,42 +108,44 @@ class MasterActor(val taskType: String, val view: ActorRef[ViewMessage], val pdf
           }
       })
 
-      if(!this.stopMonitor.isStopped) {
+      if(!stopMonitor.isStopped) {
         log("Process PDF completed")
         log("Completion arrived")
       }
 
     } catch {
-      case e: Exception => {
-        this.stopMonitor.stop()
+      case e: Exception =>
+        stopMonitor.stop()
         log("Interrupted")
         view ! ChangeState("Interrupted")
-      }
+      
     }
   }
 
-  private def mostFrequentWords(): Unit = {
+  private def mostFrequentWords(context: ActorContext[MasterMessage], nWords: Int,
+                                wordsBag: ActorRef[Command], view: ActorRef[ViewMessage],
+                                numTasks: Int, stopMonitor: StopMonitor): Unit = {
     val picker = context.spawn(PickActor("Pick Actor", nWords, wordsBag), "Picker")
 
     log("Computing most frequent words...")
     view ! ChangeState("Computing most frequent words...")
 
-    this.wordsBag ! Clear()
+    wordsBag ! Clear()
 
     // - 1 present for the pick task the map of strings
-    numTasks = numTasks - 1
+    val numTask: Int = numTasks - 1
 
     val numOfWords: Int = stringList.length
     var startingIndex: Int = 0
-    val dx: Int = numOfWords / numTasks
+    val dx: Int = numOfWords / numTask
 
-    log("List of words size: " + numOfWords, "Num of tasks: " + numTasks)
+    log("List of words size: " + numOfWords, "Num of tasks: " + numTask)
 
     var wordsResults: List[Future[Boolean]] = List.empty
-    for(i <- 0 until numTasks) {
+    for(i <- 0 until numTask) {
       try {
         val res: Future[Boolean] = executor.submit(new WordsTask("Words Task", stringList, startingIndex,
-          startingIndex + dx, wordsBag, stopMonitor));
+          startingIndex + dx, wordsBag, stopMonitor))
         wordsResults = res :: wordsResults
         startingIndex = startingIndex + dx
       } catch {
@@ -169,10 +166,10 @@ class MasterActor(val taskType: String, val view: ActorRef[ViewMessage], val pdf
     }
 
     if(!stopMonitor.isStopped) {
-      log("Wait words tasks completion");
+      log("Wait words tasks completion")
       try {
         while(!wordsResults.forall(res => res.isDone)) {
-          this.pickWordsFrequency(picker)
+          this.pickWordsFrequency(context, picker, stopMonitor, view)
           waitFor(WAITING_TIME)
         }
       } catch {
@@ -185,28 +182,29 @@ class MasterActor(val taskType: String, val view: ActorRef[ViewMessage], val pdf
         this.workCompleted = wordsResults.forall(res => {
           var result: Boolean = false
           try {
-            result = res.get();
+            result = res.get()
           } catch {
             case e: Exception => e.printStackTrace()
           }
           result
         })
 
-        this.pickWordsFrequency(picker)
+        this.pickWordsFrequency(context, picker, stopMonitor, view)
         log("Done")
       }
       else {
-        log("Interrupted");
-        view ! ChangeState("Interrupted");
+        log("Interrupted")
+        view ! ChangeState("Interrupted")
       }
     }
     else {
-      log("Interrupted");
-      view ! ChangeState("Interrupted");
+      log("Interrupted")
+      view ! ChangeState("Interrupted")
     }
   }
 
-  private def pickWordsFrequency(picker: ActorRef[Command]): Unit = {
+  private def pickWordsFrequency(context: ActorContext[MasterMessage], picker: ActorRef[Command],
+                                 stopMonitor: StopMonitor, view: ActorRef[ViewMessage]): Unit = {
     log("Picking start")
 
     implicit val timeout: Timeout = 2.seconds
@@ -216,7 +214,7 @@ class MasterActor(val taskType: String, val view: ActorRef[ViewMessage], val pdf
     implicit val ec: ExecutionContextExecutor = context.executionContext
     //remember you can't call context on future callback
     f.onComplete({
-      case Success(value) if value.isInstanceOf[MaxWordsResult] => {
+      case Success(value) if value.isInstanceOf[MaxWordsResult] =>
         var result: Option[(Integer, List[(String, Integer)])] = Option.empty
         try {
           result = value.asInstanceOf[MaxWordsResult].result
@@ -229,15 +227,15 @@ class MasterActor(val taskType: String, val view: ActorRef[ViewMessage], val pdf
             view ! ChangeState("Interrupted")
         }
 
-        if(!result.isEmpty) {
+        if(result.isDefined) {
           val wordsProcessed: Int = result.get._1
           val wordsFreq: List[(String, Integer)] = result.get._2
 
-          log("Words processed: " + wordsProcessed, wordsFreq.toString());
+          log("Words processed: " + wordsProcessed, wordsFreq.toString())
 
           view ! UpdateResult(wordsProcessed, wordsFreq, this.workCompleted)
           if(this.workCompleted) {
-            log("Most Frequent Words completed");
+            log("Most Frequent Words completed")
 
             val timeElapsed: Long = System.currentTimeMillis() - startTime
             log("Completed - time elapsed: " + timeElapsed)
@@ -245,9 +243,9 @@ class MasterActor(val taskType: String, val view: ActorRef[ViewMessage], val pdf
           }
         }
         else {
-          log("Empty bag");
+          log("Empty bag")
         }
-      }
+
       case _ => log("ERROR")
     })
   }
